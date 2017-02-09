89ca27fae4a1a87718c9ae308ce521a1689ddcf0
Aditya Goyal <aditya.g.goyal@oracle.com>
2017-02-09 13:26:12 -0500

    first test of magicfly control

--- Python/MagicFlyControl.py
+++ Python/MagicFlyControl.py
@@ -2,7 +2,38 @@
 This code contains starting python code example for arduino pymata testing.
 '''
 
-#TODO: add pin for transmission.
+import time
+import sys
+import signal
+
+from PyMata.pymata import PyMata
+
+
+# create a PyMata instance
+board = PyMata("COM", verbose=True)
+
+# you may need to press ctrl c twice
+def signal_handler(sig, frame):
+    print('You pressed Ctrl+C')
+    if board is not None:
+        board.reset()
+        board.close()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+# Configure the trigger and echo pins
+
+time.sleep(1)
+
+# Create a forever loop that will print out the sonar data for the PING device
+
+while 1:
+    data = board.digital_write(13, 1)
+    print(str(data[2]) + ' centimeters')
+    time.sleep(.2)
+
+# TODO: add pin for transmission.
 
 def magicfly_control(self, data):
     """
@@ -13,3 +44,16 @@ def magicfly_control(self, data):
     """
 
 
+def transmit_magicfly():
+    """
+    Initially.. let's just try to send a predefined stuff.
+    // stepper motor configuration message definition for 2 conductor motor
+  0  START_SYSEX                     (0xF0)
+  1  MAGICFLY_CONTROL                    (0x42)
+  2  TRANSMIT               (0x00)
+  3  PULSE_LENGTH            length of pulse
+  3  BIT_LENGTH              length of bit
+  4  DATA (BIT LENGTH number of bytes)
+  7  END_SYSEX (0xF7)
+    :return:
+    """
