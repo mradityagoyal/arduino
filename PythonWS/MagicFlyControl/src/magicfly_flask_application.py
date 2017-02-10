33075057ffcc1c715d7249f927c591e3da606621
Aditya Goyal <Aditya.G.Goyal@oracle.com>
2017-02-10 15:48:30 -0500

    finish Flask app for basic init and sending code

--- PythonWS/MagicFlyControl/src/magicfly_flask_application.py
+++ PythonWS/MagicFlyControl/src/magicfly_flask_application.py
@@ -18,6 +18,7 @@ Created on Feb 10, 2017
 from flask import Flask
 import magicfly_board
 app = Flask(__name__)
+#TODO: find a better way to maintain state. 
 data = {}
 BASE_URL = '/magicfly/api/v0.1'
 
@@ -27,20 +28,43 @@ def hello():
 
 @app.route((BASE_URL + "/init/<port>"))
 def init(port):
+    """
+    Initializes the magicfly controller on serial port port. 
+    @param port: the serial port to use.  
+    """
     print(port)
     data["board"]= magicfly_board.MagicflyBoard(port)
     return "Magicfly Board initialized. "
 
 @app.route(BASE_URL + "/echo/<message>")
-def getBoard(message):
+def test_echoMessage(message):
+    """
+    Echoes a message all way through to the magicfly_board. 
+    """
     board =  data["board"]
+    #Find a way to cast as type. 
     return board.echo_ping(message)
 
 @app.route(BASE_URL + "/transmit/<code>")
 def transmitCode(code):
+    """
+    Transmits the binary code using the magicfly board object. 
+    @param code: the binary code to send.  
+    """
     board =  data["board"]
     board.sendCode(code)
     return "code sent: {}".format(code)
+
+@app.route(BASE_URL + "/pulse/set/<value>")
+def set_pulse_width(value):
+    """
+    Sets the pulse width on the board
+    @param value: the pulsewidth to use in the board.  
+    """
+    board =  data["board"]
+    board.set_pulse_width(value)
+    return "pulse_width set to: {}".format(value)
+    
 
 if __name__ == "__main__":
     app.run()
