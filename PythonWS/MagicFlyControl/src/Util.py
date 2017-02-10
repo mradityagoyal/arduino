aa60c92e64b46c69d35fef941763014add6e607f
Aditya Goyal <Aditya.G.Goyal@oracle.com>
2017-02-10 13:08:54 -0500

    Create new control class, start

---
+++ PythonWS/MagicFlyControl/src/Util.py
@@ -0,0 +1,9 @@
+def toFirmataFormat(str):
+    data = bytearray()
+    for b in str.encode('utf8'):
+        data.append(b & 0x7F)
+        data.append((b >> 7) & 0x7F)
+    return data
+
+def long_to_bytes (val):
+    return [val & 0x7f, (val & 0x7F00)>>7, (val & 0x7f0000) >> 16, (val & 0x7f0000)>>24]
---
+++ PythonWS/MagicFlyControl/src/control.py
@@ -0,0 +1,48 @@
+'''
+Created on Feb 10, 2017
+
+@author: agoyal
+
+
+'''
+
+import time
+from PyMata import pymata as pm
+
+class MagicflyBoard:
+    BOARD_LED = 13
+    MAGICFLY_TX_CODE = 0x42
+    MAGICFLY_SET_PULSE_WIDTH = 0x43 # sets the pulse width. TODO:
+    
+    
+    def __init__(self, port):
+#         print("MagicflyBoard init")
+        self.board = pm.PyMata(port, verbose=True)
+    
+    def sendCode(self, code):
+        """
+        Sends the code to magicfly receivers. 
+        @param code: the binary code to send.  
+        """
+        # Set the output to 1 = High
+        self.board.digital_write(self.BOARD_LED, 1)
+        self.board._command_handler.send_sysex(self.MAGICFLY_TX_CODE, code.encode('utf8'))
+        # Wait a half second between toggles.
+        time.sleep(.5)
+        # Set the output to 0 = Low
+        self.board.digital_write(self.BOARD_LED, 0)
+        time.sleep(.5)
+        
+    def set_pulse_width(self, pulseWidth):
+        """
+        Sends the pulsewidth to use in rcswitch to the arduino. 
+        """
+        self.board._command_handler.send_sysex(self.MAGICFLY_SET_PULSE_WIDTH, pulseWidth)
+    
+    def echo_ping(self, msg):
+        print("echoing: {}".format(msg))
+#         self.port = port
+
+
+mfBrd = MagicflyBoard("Haha")
+mfBrd.echo_ping("hellow World")
